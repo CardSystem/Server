f50ab4bf4904e87f1f66dde9ec27d164d962e1ca
alivejuicy <kkddhh1998@gmail.com>
2023-06-15 21:32:16 +0900

    #7 refactor: 카드 상품 entity, dto로 분리

--- src/main/java/domain/Product.java
+++ src/main/java/domain/Product.java
@@ -1,5 +1,11 @@
 package domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class Product {
 	private Long productId;
 	private String cardName;
@@ -7,49 +13,12 @@ public class Product {
 	private Long cardLimit;
 	private Long categoryId;
 
-	public Product() {
-
-	}
-
-	public Product(Long productId, String cardName, String cardType, Long cardLimit, Long categoryId) {
-		super(); /// 부모 클래스 생성자 호출
-		this.productId = productId;
+	@Builder
+	public Product(String cardName, String cardType, Long cardLimit, Long categoryId) {
+		//super(); /// 부모 클래스 생성자 호출
 		this.cardName = cardName;
 		this.cardType = cardType;
 		this.cardLimit = cardLimit;
 		this.categoryId = categoryId;
 	}
-
-	public String getCardName() {
-		return cardName;
-	}
-
-	public void setCardName(String cardName) {
-		this.cardName = cardName;
-	}
-
-	public String getCardType() {
-		return cardType;
-	}
-
-	public void setCardType(String cardType) {
-		this.cardType = cardType;
-	}
-
-	public Long getCardLimit() {
-		return cardLimit;
-	}
-
-	public void setCardLimit(Long cardLimit) {
-		this.cardLimit = cardLimit;
-	}
-
-	public Long getCategoryId() {
-		return categoryId;
-	}
-
-	public void setCategoryId(Long categoryId) {
-		this.categoryId = categoryId;
-	}
-
 }
--- src/main/java/domain/ProductDTO.java
+++ src/main/java/domain/ProductDTO.java
@@ -1,5 +1,21 @@
 package domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class ProductDTO {
+
+	@Data
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class RequestProduct {
+		private String cardName;
+		private String cardType;
+		private Long cardLimit;
+		private Long categoryId;
+	}
 
 }
