8ddb62a52305e38a738a07fc427d6048252f7872
alivejuicy <kkddhh1998@gmail.com>
2023-06-30 19:35:30 +0900

    feat: 상품 RequestDto, ResponseDto 최종 Merge

---
+++ src/main/java/dto/ProductResponseDto.java
@@ -0,0 +1,33 @@
+package dto;
+
+import domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponseDto {
+	private Long id;
+	private String cardName;
+	private String cardType;
+	private Long cardLimit;
+	private Long categoryId;
+	private String categoryName;
+	private Long discount;
+	
+	public static ProductResponseDto of(Product product) {
+		return ProductResponseDto.builder()
+				.id(product.getId())
+				.cardName(product.getCardName())
+				.cardType(product.getCardType())
+				.cardLimit(product.getCardLimit())
+				.categoryId(product.getCategoryId())
+				.categoryName(product.getCategoryName())
+				.discount(product.getDiscount())
+				.build();				
+	}
+}
