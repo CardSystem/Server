98340e7642dffc1e37e1911a679b8fa5dca76338
alivejuicy <kkddhh1998@gmail.com>
2023-07-01 11:52:46 +0900

    feat: 에러코드 enum 추가, 카드 상품 서비스 테스트 코드 성공/실패 케이스 최종 구현 완료

--- src/main/java/exception/ErrorCode.java
+++ src/main/java/exception/ErrorCode.java
@@ -5,8 +5,10 @@ public enum ErrorCode {
 	SUCCESS(200, "OK", "성공"), INSUFFICIENT_BALANCE(500, "Internal Server Error", "잔액이 부족합니다"),
 	STOPPED_ACCOUNT(500, "Internal Server Error", "정지된 계좌입니다."),
 	STOPPED_CARD(500, "Internal Server Error", "정지된 카드입니다."),
-	UNABLE_CARDNUM(500, "Internal Server Error", "존재하지 않는 카드번호입니다.");
-
+	UNABLE_CARDNUM(500, "Internal Server Error", "존재하지 않는 카드번호입니다."),
+	UNABLE_PRODUCTNUM(500, "Internal Server Error", "존재하지 않는 상품입니다."),
+	DUPLICATE_PRODUCT(500, "Internal Server Error", "상품명이 중복된 상품입니다."),
+	NOT_FOUND_PRODUCTLIST(500, "Internal Server Error", "상품 리스트를 찾을 수 없습니다.");
 	public String getStatus() {
 		return status;
 	}
